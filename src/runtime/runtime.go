5d1f8cd552b9dfc348423d606e17d53a34c8f0ab
warrenwyf <warrenwyf@gmail.com>
2018-03-24 21:52:08 +0800

    Make absolute path readable in runtime package

--- src/runtime/runtime.go
+++ src/runtime/runtime.go
@@ -1,8 +1,36 @@
 package runtime
 
+import (
+	"path"
+)
+
 var (
 	ConfigFile string
 	DataDir    string
 	LogDir     string
 	WorkingDir string
 )
+
+func GetAbsConfigFile() string {
+	if path.IsAbs(ConfigFile) {
+		return ConfigFile
+	}
+
+	return path.Join(WorkingDir, ConfigFile)
+}
+
+func GetAbsDataDir() string {
+	if path.IsAbs(DataDir) {
+		return DataDir
+	}
+
+	return path.Join(WorkingDir, DataDir)
+}
+
+func GetAbsLogDir() string {
+	if path.IsAbs(LogDir) {
+		return LogDir
+	}
+
+	return path.Join(WorkingDir, LogDir)
+}
